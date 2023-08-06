f004761821e70eec938cc03b19a343f8a64e9b39
jimjam197 <Sjc97@live.com>
2023-08-06 18:21:56 -0400

    added route for login

--- app.js
+++ app.js
@@ -17,3 +17,11 @@ app.use(express.json());
 //! Routes
 
 app.listen(PORT, () => log(`Chat server is running on port : ${PORT}`));
+
+const express = require('express');
+const router = express.Router();
+const { loginUser } = require('../controllers/userController');
+
+router.post('/login', loginUser);
+
+module.exports = router;
