a53b5d1605ee64061e32627bf3c9b1ff32c63ad6
jimjam197 <Sjc97@live.com>
2023-08-07 18:30:25 -0400

    updating just tio push

--- models/messageRooms.models.js
+++ models/messageRooms.models.js
@@ -1,4 +1,27 @@
+<<<<<<< Updated upstream
 /*
  | title | description | messages | ownerId |
 | --- | --- | --- | ---|
 | required / unique | not required | array | id | */
+=======
+const mongoose = require("mongoose");
+
+const MessageRoomSchema = new mongoose.schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  messages: {
+    type: Array,
+  },
+  ownerID: {
+    type: id,
+  },
+});
+
+module.exports = mongoose.model("Room", MessageRoomSchema);
+>>>>>>> Stashed changes
